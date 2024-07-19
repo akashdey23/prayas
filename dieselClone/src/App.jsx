b145024a1eb4df052a4477fe02c707524e6b95fa
sadiquafarheen2003 <worksadiqua@gmail.com>
2024-07-19 23:40:20 +0530

    new-vite-created

---
+++ dieselClone/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>this is react proj</h1>
+        
+    </>
+  )
+}
+
+export default App
