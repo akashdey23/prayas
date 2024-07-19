cc84c9bfc098b6234494e9cd8548b409b98a5ab4
sadiquafarheen2003 <worksadiqua@gmail.com>
2024-07-20 00:06:10 +0530

    new-vite-proj

--- dieselClone/src/App.jsx
+++ dieselClone/src/App.jsx
@@ -1,13 +1,12 @@
 import { useState } from 'react'
-import './App.css'
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>this is react proj</h1>
-        
+      <h1>react project</h1>
     </>
   )
 }
