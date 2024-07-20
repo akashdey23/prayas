12c10f01b24452ae69b678972ba7c95fc724bb72
Ayush-silicon <ayush.singh.kv26@gmail.com>
2024-07-20 23:29:11 +0530

    Ayush-added-some files

---
+++ dieselClone/src/Components_Ayush/wishlist.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './wishlist.css';
+
+const Wishlist = () => {
+  return (
+    <div className="wishlist-container">
+      <h1>My Wishlist</h1>
+      <p>Your wishlist is currently empty. Start adding your favorite items!</p>
+    </div>
+  );
+};
+
+export default Wishlist;
