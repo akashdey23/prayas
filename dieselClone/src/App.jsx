2e83c00de891d7d83923e64a6eb1cd95cc13154e
Ayush-silicon <ayush.singh.kv26@gmail.com>
2024-07-21 18:57:28 +0530

    Ayush-COMMIT

--- dieselClone/src/App.jsx
+++ dieselClone/src/App.jsx
@@ -11,11 +11,8 @@ const App = () => {
     <div className="App">
       <Navbar />
     </div>
-<<<<<<< HEAD
 
 
-=======
->>>>>>> dd6abc29ab092ef09d518428b2219f8f4024b73a
     
       <h1>react project</h1>
       <Footer/>
