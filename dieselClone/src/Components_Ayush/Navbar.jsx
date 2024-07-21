1902f088a8b8dfb065de501afa313f28d12f40dc
Ayush-silicon <ayush.singh.kv26@gmail.com>
2024-07-21 10:30:40 +0530

    Ayush-added-some files

--- dieselClone/src/Components_Ayush/Navbar.jsx
+++ dieselClone/src/Components_Ayush/Navbar.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+
 import './Navbar.css';
 
 
