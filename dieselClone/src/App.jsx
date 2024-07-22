69bfb0cb1312987bb6f1882efdbdae142a2f841e
sadiquafarheen2003 <worksadiqua@gmail.com>
2024-07-22 21:21:54 +0530

    overlays done

--- dieselClone/src/App.jsx
+++ dieselClone/src/App.jsx
@@ -1,11 +1,12 @@
 // App.jsx
 import React from 'react';
+import { BrowserRouter as Router,Route,Routes } from 'react-router-dom';
 import Navbar from './Components_Ayush/Navbar';
 import Login from './Components_Ipsa/Login';
 import Footer from './components_sadiqua/Footer'
 import { Wishlist } from './Components_Ipsa/Wishlist';
 import { Cart } from './Components_Ipsa/Cart';
-
+import Overlay from './Components_Jaydip/Overlay';
 
 const App = () => {
   return (
@@ -13,10 +14,11 @@ const App = () => {
     <div className="App">
       <Navbar />
     </div>
-
-
-    
-      <h1>react project</h1>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Overlay />} />
+      </Routes>
+    </Router>
       <Login/>
       <Wishlist/>
       <Cart/>
--- dieselClone/src/Components_Jaydip/Banner.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export const Banner = () => {
-  return (
-    <div>Banner</div>
-  )
-}
