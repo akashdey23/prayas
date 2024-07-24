572fe0d3b932865852916a4a00d4815204e4b81f
Arjit-dev <m.arjit2005@gmail.com>
2024-07-24 19:25:21 +0530

    Modify

--- dieselClone/src/Components_Akash/Akash.jsx
+++ dieselClone/src/Components_Akash/Akash.jsx
@@ -1,10 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import './Akash.css';
 import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
 
 import Product from "./Product";
 export default function Akash() {
+  
     const responsive = {
         superLargeDesktop: {
           // the naming can be any, depends on you.
@@ -27,235 +28,235 @@ export default function Akash() {
 
       const productData = [
         {
-            id : 1,
-            name : "Designer Denim fit for Men",
-            price :9999,
-            "newprice": 8999,
-            description: "Your perfect pack for everyday use and walks in the forest. Stash your style and swag in your everyday fits",
-            "category": "men_den",
-            imageurl: "../assets/Images/denim_img_men1.jpg",
-            "rating": {
-              "rate": 3.9,
-              "count": 120
-            },
-            "stock": 327,
-            "pdetails":{
-                "Material": "61%Leather,39%Cotton",
-                "Color": "Yellow",
-                "Occasion": "Casual"
-            },
-            "Rtm": "Return Time Limit: Easy 30 days return",
-            "otdetails": {
-                "csd": "Support Number: 022- 48930102, Support Email: support@dieselindia.com",
-                "mib": "Diesel Fashion India Reliance Pvt. Ltd., 8th Floor, Maker Tower E, Cuffe Parade, Mumbai, Maharashtra, 400005.",
-                "nq": "1 N",
-                "coo": "VIETNAM",
-                "noc": "DENIM",
-                "sc": "Y03204 P5578"
-            }
+          "id": 1,
+          "title": "Designer Denim fit for Men",
+          "oldprice":9999,
+          "newprice": 8999,
+          "description": "Your perfect pack for everyday use and walks in the forest. Stash your style and swag in your everyday fits",
+          "category": "men_den",
+          "image": "Images/denim_img_men1.jpg",
+          "rating": {
+            "rate": 3.9,
+            "count": 120
           },
-          {
-            id: 2,
-            name: "Designer Denim fit for Men",
-            price: 7999,
-            "newprice": 4999,
-            description: "Your perfect pack for everyday use and walks in the forest. Stash your style and swag in your everyday fits",
-            "category": "men_den",
-            imageurl: "../assets/Images/denim_img_men2.jpg",
-            "rating": {
-              "rate": 3.9,
-              "count": 120
-            },
-            "stock": 327,
-            "pdetails":{
-                "Material": "61%Leather,39%Cotton",
-                "Color": "Yellow",
-                "Occasion": "Casual"
-            },
-            "Rtm": "Return Time Limit: Easy 30 days return",
-            "otdetails": {
-                "csd": "Support Number: 022- 48930102, Support Email: support@dieselindia.com",
-                "mib": "Diesel Fashion India Reliance Pvt. Ltd., 8th Floor, Maker Tower E, Cuffe Parade, Mumbai, Maharashtra, 400005.",
-                "nq": "1 N",
-                "coo": "VIETNAM",
-                "noc": "DENIM",
-                "sc": "Y03204 P5578"
-            }
+          "stock": 327,
+          "pdetails":{
+              "Material": "61%Leather,39%Cotton",
+              "Color": "Yellow",
+              "Occasion": "Casual"
           },
-          {
-            id: 3,
-            name: "Designer Denim fit for Men",
-            price: 2999,
-            "newprice": 1999,
-            description: "Your perfect pack for everyday use and walks in the forest. Stash your style and swag in your everyday fits",
-            "category": "men_den",
-            imageurl: "../assets/Images/denim_img_men2.jpg",
-            "rating": {
-              "rate": 3.9,
-              "count": 120
-            },
-            "stock": 327,
-            "pdetails":{
-                "Material": "61%Leather,39%Cotton",
-                "Color": "Yellow",
-                "Occasion": "Casual"
-            },
-            "Rtm": "Return Time Limit: Easy 30 days return",
-            "otdetails": {
-                "csd": "Support Number: 022- 48930102, Support Email: support@dieselindia.com",
-                "mib": "Diesel Fashion India Reliance Pvt. Ltd., 8th Floor, Maker Tower E, Cuffe Parade, Mumbai, Maharashtra, 400005.",
-                "nq": "1 N",
-                "coo": "VIETNAM",
-                "noc": "DENIM",
-                "sc": "Y03204 P5578"
-            }
+          "Rtm": "Return Time Limit: Easy 30 days return",
+          "otdetails": {
+              "csd": "Support Number: 022- 48930102, Support Email: support@dieselindia.com",
+              "mib": "Diesel Fashion India Reliance Pvt. Ltd., 8th Floor, Maker Tower E, Cuffe Parade, Mumbai, Maharashtra, 400005.",
+              "nq": "1 N",
+              "coo": "VIETNAM",
+              "noc": "DENIM",
+              "sc": "Y03204 P5578"
+          }
+        },
+        {
+          "id": 2,
+          "title": "Designer Denim fit for Men",
+          "oldprice": 7999,
+          "newprice": 4999,
+          "description": "Your perfect pack for everyday use and walks in the forest. Stash your style and swag in your everyday fits",
+          "category": "men_den",
+          "image": "Images/denim_img_men2.jpg",
+          "rating": {
+            "rate": 3.9,
+            "count": 120
           },
-          {
-            id: 4,
-            name: "Designer Denim fit for Men",
-            price:5999,
-            "newprice": 4999,
-            description: "Your perfect pack for everyday use and walks in the forest. Stash your style and swag in your everyday fits",
-            "category": "men_den",
-            imageurl: "../assets/Images/denim_img_men4.jpg",
-            "rating": {
-              "rate": 3.9,
-              "count": 120
-            },
-            "stock": 327,
-            "pdetails":{
-                "Material": "61%Leather,39%Cotton",
-                "Color": "Yellow",
-                "Occasion": "Casual"
-            },
-            "Rtm": "Return Time Limit: Easy 30 days return",
-            "otdetails": {
-                "csd": "Support Number: 022- 48930102, Support Email: support@dieselindia.com",
-                "mib": "Diesel Fashion India Reliance Pvt. Ltd., 8th Floor, Maker Tower E, Cuffe Parade, Mumbai, Maharashtra, 400005.",
-                "nq": "1 N",
-                "coo": "VIETNAM",
-                "noc": "DENIM",
-                "sc": "Y03204 P5578"
-            }
+          "stock": 327,
+          "pdetails":{
+              "Material": "61%Leather,39%Cotton",
+              "Color": "Yellow",
+              "Occasion": "Casual"
           },
-          {
-            id: 5,
-            name: "Designer Denim fit for Men",
-            price:9999,
-            "newprice": 8999,
-            description: "Your perfect pack for everyday use and walks in the forest. Stash your style and swag in your everyday fits",
-            "category": "men_den",
-            imageurl: "../assets/Images/denim_img_men5.jpg",
-            "rating": {
-              "rate": 3.9,
-              "count": 120
-            },
-            "stock": 327,
-            "pdetails":{
-                "Material": "61%Leather,39%Cotton",
-                "Color": "Yellow",
-                "Occasion": "Casual"
-            },
-            "Rtm": "Return Time Limit: Easy 30 days return",
-            "otdetails": {
-                "csd": "Support Number: 022- 48930102, Support Email: support@dieselindia.com",
-                "mib": "Diesel Fashion India Reliance Pvt. Ltd., 8th Floor, Maker Tower E, Cuffe Parade, Mumbai, Maharashtra, 400005.",
-                "nq": "1 N",
-                "coo": "VIETNAM",
-                "noc": "DENIM",
-                "sc": "Y03204 P5578"
-            }
+          "Rtm": "Return Time Limit: Easy 30 days return",
+          "otdetails": {
+              "csd": "Support Number: 022- 48930102, Support Email: support@dieselindia.com",
+              "mib": "Diesel Fashion India Reliance Pvt. Ltd., 8th Floor, Maker Tower E, Cuffe Parade, Mumbai, Maharashtra, 400005.",
+              "nq": "1 N",
+              "coo": "VIETNAM",
+              "noc": "DENIM",
+              "sc": "Y03204 P5578"
+          }
+        },
+        {
+          "id": 3,
+          "title": "Designer Denim fit for Men",
+          "oldprice": 2999,
+          "newprice": 1999,
+          "description": "Your perfect pack for everyday use and walks in the forest. Stash your style and swag in your everyday fits",
+          "category": "men_den",
+          "image": "Images/denim_img_men3.jpg",
+          "rating": {
+            "rate": 3.9,
+            "count": 120
           },
-          {
-            id: 6,
-            name: "Designer Denim fit for Men",
-            price: 3999,
-            "newprice": 2999,
-            description: "hiiiiiii dickhead",
-            "category": "men_den",
-            imageurl: "../assets/Images/denim_img_men6.jpg",
-            "rating": {
-              "rate": 3.9,
-              "count": 120
-            },
-            "stock": 37,
-            "pdetails":{
-                "Material": "61%Leather,39%Cotton",
-                "Color": "Yellow",
-                "Occasion": "Casual"
-            },
-            "Rtm": "Return Time Limit: Easy 30 days return",
-            "otdetails": {
-                "csd": "Support Number: 022- 48930102, Support Email: support@dieselindia.com",
-                "mib": "Diesel Fashion India Reliance Pvt. Ltd., 8th Floor, Maker Tower E, Cuffe Parade, Mumbai, Maharashtra, 400005.",
-                "nq": "1 N",
-                "coo": "VIETNAM",
-                "noc": "DENIM",
-                "sc": "Y03204 P5578"
-            }
+          "stock": 327,
+          "pdetails":{
+              "Material": "61%Leather,39%Cotton",
+              "Color": "Yellow",
+              "Occasion": "Casual"
           },
-          {
-            id: 7,
-            name: "Designer Denim fit for Men",
-            price:7999,
-            "newprice": 6999,
-            description: "Your perfect pack for everyday use and walks in the forest. Stash your style and swag in your everyday fits",
-            "category": "men_den",
-            imageurl: "../assets/Images/denim_img_men7.jpg",
-            "rating": {
-              "rate": 3.9,
-              "count": 120
-            },
-            "stock": 327,
-            "pdetails":{
-                "Material": "61%Leather,39%Cotton",
-                "Color": "Yellow",
-                "Occasion": "Casual"
-            },
-            "Rtm": "Return Time Limit: Easy 30 days return",
-            "otdetails": {
-                "csd": "Support Number: 022- 48930102, Support Email: support@dieselindia.com",
-                "mib": "Diesel Fashion India Reliance Pvt. Ltd., 8th Floor, Maker Tower E, Cuffe Parade, Mumbai, Maharashtra, 400005.",
-                "nq": "1 N",
-                "coo": "VIETNAM",
-                "noc": "DENIM",
-                "sc": "Y03204 P5578"
-            }
+          "Rtm": "Return Time Limit: Easy 30 days return",
+          "otdetails": {
+              "csd": "Support Number: 022- 48930102, Support Email: support@dieselindia.com",
+              "mib": "Diesel Fashion India Reliance Pvt. Ltd., 8th Floor, Maker Tower E, Cuffe Parade, Mumbai, Maharashtra, 400005.",
+              "nq": "1 N",
+              "coo": "VIETNAM",
+              "noc": "DENIM",
+              "sc": "Y03204 P5578"
+          }
+        },
+        {
+          "id": 4,
+          "title": "Designer Denim fit for Men",
+          "oldprice":5999,
+          "newprice": 4999,
+          "description": "Your perfect pack for everyday use and walks in the forest. Stash your style and swag in your everyday fits",
+          "category": "men_den",
+          "image": "Images/denim_img_men4.jpg",
+          "rating": {
+            "rate": 3.9,
+            "count": 120
           },
-          {
-            id: 8,
-            name: "Designer Denim fit for Men",
-            price:10999,
-            "newprice": 8999,
-            description: "Your perfect pack for everyday use and walks in the forest. Stash your style and swag in your everyday fits",
-            "category": "men_den",
-            imageurl: "../assets/Images/denim_img_men8.jpg",
-            "rating": {
-              "rate": 3.9,
-              "count": 120
-            },
-            "stock": 327,
-            "pdetails":{
-                "Material": "61%Leather,39%Cotton",
-                "Color": "Yellow",
-                "Occasion": "Casual"
-            },
-            "Rtm": "Return Time Limit: Easy 30 days return",
-            "otdetails": {
-                "csd": "Support Number: 022- 48930102, Support Email: support@dieselindia.com",
-                "mib": "Diesel Fashion India Reliance Pvt. Ltd., 8th Floor, Maker Tower E, Cuffe Parade, Mumbai, Maharashtra, 400005.",
-                "nq": "1 N",
-                "coo": "VIETNAM",
-                "noc": "DENIM",
-                "sc": "Y03204 P5578"
-            }
+          "stock": 327,
+          "pdetails":{
+              "Material": "61%Leather,39%Cotton",
+              "Color": "Yellow",
+              "Occasion": "Casual"
           },
+          "Rtm": "Return Time Limit: Easy 30 days return",
+          "otdetails": {
+              "csd": "Support Number: 022- 48930102, Support Email: support@dieselindia.com",
+              "mib": "Diesel Fashion India Reliance Pvt. Ltd., 8th Floor, Maker Tower E, Cuffe Parade, Mumbai, Maharashtra, 400005.",
+              "nq": "1 N",
+              "coo": "VIETNAM",
+              "noc": "DENIM",
+              "sc": "Y03204 P5578"
+          }
+        },
+        {
+          "id": 5,
+          "title": "Designer Denim fit for Men",
+          "oldprice":9999,
+          "newprice": 8999,
+          "description": "Your perfect pack for everyday use and walks in the forest. Stash your style and swag in your everyday fits",
+          "category": "men_den",
+          "image": "Images/denim_img_men5.jpg",
+          "rating": {
+            "rate": 3.9,
+            "count": 120
+          },
+          "stock": 327,
+          "pdetails":{
+              "Material": "61%Leather,39%Cotton",
+              "Color": "Yellow",
+              "Occasion": "Casual"
+          },
+          "Rtm": "Return Time Limit: Easy 30 days return",
+          "otdetails": {
+              "csd": "Support Number: 022- 48930102, Support Email: support@dieselindia.com",
+              "mib": "Diesel Fashion India Reliance Pvt. Ltd., 8th Floor, Maker Tower E, Cuffe Parade, Mumbai, Maharashtra, 400005.",
+              "nq": "1 N",
+              "coo": "VIETNAM",
+              "noc": "DENIM",
+              "sc": "Y03204 P5578"
+          }
+        },
+        {
+          "id": 6,
+          "title": "Designer Denim fit for Men",
+          "oldprice": 3999,
+          "newprice": 2999,
+          "description": "Your perfect pack for everyday use and walks in the forest. Stash your style and swag in your everyday fits",
+          "category": "men_den",
+          "image": "Images/denim_img_men6.jpg",
+          "rating": {
+            "rate": 3.9,
+            "count": 120
+          },
+          "stock": 37,
+          "pdetails":{
+              "Material": "61%Leather,39%Cotton",
+              "Color": "Yellow",
+              "Occasion": "Casual"
+          },
+          "Rtm": "Return Time Limit: Easy 30 days return",
+          "otdetails": {
+              "csd": "Support Number: 022- 48930102, Support Email: support@dieselindia.com",
+              "mib": "Diesel Fashion India Reliance Pvt. Ltd., 8th Floor, Maker Tower E, Cuffe Parade, Mumbai, Maharashtra, 400005.",
+              "nq": "1 N",
+              "coo": "VIETNAM",
+              "noc": "DENIM",
+              "sc": "Y03204 P5578"
+          }
+        },
+        {
+          "id": 7,
+          "title": "Designer Denim fit for Men",
+          "oldprice":7999,
+          "newprice": 6999,
+          "description": "Your perfect pack for everyday use and walks in the forest. Stash your style and swag in your everyday fits",
+          "category": "men_den",
+          "image": "Images/denim_img_men7.jpg",
+          "rating": {
+            "rate": 3.9,
+            "count": 120
+          },
+          "stock": 327,
+          "pdetails":{
+              "Material": "61%Leather,39%Cotton",
+              "Color": "Yellow",
+              "Occasion": "Casual"
+          },
+          "Rtm": "Return Time Limit: Easy 30 days return",
+          "otdetails": {
+              "csd": "Support Number: 022- 48930102, Support Email: support@dieselindia.com",
+              "mib": "Diesel Fashion India Reliance Pvt. Ltd., 8th Floor, Maker Tower E, Cuffe Parade, Mumbai, Maharashtra, 400005.",
+              "nq": "1 N",
+              "coo": "VIETNAM",
+              "noc": "DENIM",
+              "sc": "Y03204 P5578"
+          }
+        },
+        {
+          "id": 8,
+          "title": "Designer Denim fit for Men",
+          "oldprice":10999,
+          "newprice": 8999,
+          "description": "Your perfect pack for everyday use and walks in the forest. Stash your style and swag in your everyday fits",
+          "category": "men_den",
+          "image": "Images/denim_img_men8.jpg",
+          "rating": {
+            "rate": 3.9,
+            "count": 120
+          },
+          "stock": 327,
+          "pdetails":{
+              "Material": "61%Leather,39%Cotton",
+              "Color": "Yellow",
+              "Occasion": "Casual"
+          },
+          "Rtm": "Return Time Limit: Easy 30 days return",
+          "otdetails": {
+              "csd": "Support Number: 022- 48930102, Support Email: support@dieselindia.com",
+              "mib": "Diesel Fashion India Reliance Pvt. Ltd., 8th Floor, Maker Tower E, Cuffe Parade, Mumbai, Maharashtra, 400005.",
+              "nq": "1 N",
+              "coo": "VIETNAM",
+              "noc": "DENIM",
+              "sc": "Y03204 P5578"
+          }
+        }
       ]
 
       const product = productData.map((item) => (
         <Product 
            name = {item.name} 
-           url = {item.imageurl}
+           url = {item.image}
            price = {item.price}
            description = {item.description}
         />
